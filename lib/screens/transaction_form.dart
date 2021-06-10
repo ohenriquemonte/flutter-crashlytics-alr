e5e6eb53c5e24489aca2f78ad6c749ebd88175d1
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-06-10 17:16:47 -0300

    04.05 - Toast

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -9,6 +9,7 @@ import 'package:alura_crashlytics/models/transaction.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:toast/toast.dart';
 
 class TransactionForm extends StatefulWidget {
   final Contact contact;
@@ -182,12 +183,19 @@ class _TransactionFormState extends State<TransactionForm> {
     BuildContext context, {
     String message = 'Unknown error',
   }) {
-    final snackBar = SnackBar(content: Text(message));
-    _scaffoldKey.currentState.showSnackBar(snackBar);
+    showToast(message, gravity: Toast.BOTTOM);
+
+    // final snackBar = SnackBar(content: Text(message));
+    // _scaffoldKey.currentState.showSnackBar(snackBar);
+
     // showDialog(
     //     context: context,
     //     builder: (contextDialog) {
     //       return FailureDialog(message);
     //     });
+  }
+
+  void showToast(String msg, {int duration = 5, int gravity}) {
+    Toast.show(msg, context, duration: duration, gravity: gravity);
   }
 }
